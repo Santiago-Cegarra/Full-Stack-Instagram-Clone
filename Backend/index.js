d4d7a61d5d1899143fbe48106fb0ab684684b36a
Santiago-Cegarra <santicegarra12@gmail.com>
2025-08-02 21:54:30 -0400

    Inicializacion modulos

---
+++ Backend/index.js
@@ -0,0 +1,33 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import { connect } from "mongoose";
+import connectDB from "./utils/db.js"
+
+dotenv.config({})
+const app = express()
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req,res) =>{
+    return res.status(200).json({
+        message:"Im coming from Backend",
+        success:true
+    })
+})
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(urlencoded({extended:true}));
+const corsOptions = {
+    origin: "htpp://localhost:5173",
+    credentials: true
+}
+
+app.use(cors(corsOptions));
+
+app.listen(PORT,()=>{
+    connectDB();
+    console.log(`Server Listen at Port: ${PORT}`)
+})
